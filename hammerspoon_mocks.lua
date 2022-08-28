0aac20a4b8b85c3390b155221ca42c51218e8d4b
Peter Klijn <pjmklijn@gmail.com>
2022-08-28 11:44:35 +0100

    Add some tests for increasing and decreasing windows sticking to the sides

--- hammerspoon_mocks.lua
+++ hammerspoon_mocks.lua
@@ -11,8 +11,42 @@ function hotkey.bind(mods, key, fn)
     table.insert(hotkey.bindings, { mods, key, fn })
 end
 
+local screen = {
+    rect = { x = 0, y = 0, w = 1200, h = 800 },
+}
+
+function screen:frame()
+    return self.rect
+end
+
+local window = {
+    rect = { x = 0, y = 0, w = 1200, h = 800 },
+    _screen = screen,
+}
+
+function window.focusedWindow()
+    return window
+end
+
+function window:frame()
+    return self.rect
+end
+
+function window:screen()
+    return self._screen
+end
+
+function window:move(rect, _, _, _)
+    lu.assertIsNumber(rect.x)
+    lu.assertIsNumber(rect.y)
+    lu.assertIsNumber(rect.w)
+    lu.assertIsNumber(rect.h)
+    self.rect = rect
+end
+
 local mocks = {
-    hotkey = hotkey
+    hotkey = hotkey,
+    window = window,
 }
 
 function mocks:reset()
--- init_test.lua
+++ init_test.lua
@@ -8,11 +8,11 @@ local hsmocks = require('hammerspoon_mocks')
 TestShiftIt = {} -- luacheck: ignore 111
 
 function TestShiftIt.setUp()
+    shiftit.hs = hsmocks
     hsmocks:reset()
 end
 
 function TestShiftIt.testBindDefault()
-    shiftit.hs = hsmocks
     shiftit:bindHotkeys({})
     lu.assertEquals(#hsmocks.hotkey.bindings, 16)
 
@@ -29,7 +29,6 @@ function TestShiftIt.testBindDefault()
 end
 
 function TestShiftIt.testBindOverrideVimKeys()
-    shiftit.hs = hsmocks
     shiftit:bindHotkeys({
         left = { { 'ctrl', 'alt', 'cmd' }, 'h' },
         down = { { 'ctrl', 'alt', 'cmd' }, 'j' },
@@ -47,6 +46,122 @@ function TestShiftIt.testBindOverrideVimKeys()
     for i, item in pairs(expected) do
         lu.assertEquals(hsmocks.hotkey.bindings[i][1], shiftit.mash)
         lu.assertEquals(hsmocks.hotkey.bindings[i][2], item)
+    end
+end
+
+function TestShiftIt.testResizeWindowInStepsStickingToSides()
+    local tests = {
+        {
+            desc = 'increase window sticking to left',
+            before = { x = 0, y = 0, w = 600, h = 800 },
+            expect = { x = 0, y = 0, w = 700, h = 800 },
+            increase = true,
+        },
+        {
+            desc = 'decrease window sticking to left',
+            before = { x = 0, y = 0, w = 600, h = 800 },
+            expect = { x = 0, y = 0, w = 500, h = 800 },
+            increase = false,
+        },
+        {
+            desc = 'increase window sticking to right',
+            before = { x = 600, y = 0, w = 600, h = 800 },
+            expect = { x = 500, y = 0, w = 700, h = 800 },
+            increase = true,
+        },
+        {
+            desc = 'decrease window sticking to right',
+            before = { x = 600, y = 0, w = 600, h = 800 },
+            expect = { x = 700, y = 0, w = 500, h = 800 },
+            increase = false,
+        },
+        {
+            desc = 'increase window sticking to top',
+            before = { x = 0, y = 0, w = 1200, h = 400 },
+            expect = { x = 0, y = 0, w = 1200, h = 466 },
+            increase = true,
+        },
+        {
+            desc = 'decrease window sticking to top',
+            before = { x = 0, y = 0, w = 1200, h = 400 },
+            expect = { x = 0, y = 0, w = 1200, h = 334 },
+            increase = false,
+        },
+        {
+            desc = 'increase window sticking to bottom',
+            before = { x = 0, y = 400, w = 1200, h = 400 },
+            expect = { x = 0, y = 334, w = 1200, h = 466 },
+            increase = true,
+        },
+        {
+            desc = 'decrease window sticking to bottom',
+            before = { x = 0, y = 400, w = 1200, h = 400 },
+            expect = { x = 0, y = 466, w = 1200, h = 334 },
+            increase = false,
+        },
+    }
+    for _, test in pairs(tests) do
+        hsmocks.window.rect = test.before
+        shiftit:resizeWindowInSteps(test.increase)
+        lu.assertEquals(hsmocks.window.rect, test.expect, test.desc)
+    end
+end
+
+function TestShiftIt.testResizeWindowInStepsStickingToCorners()
+    local tests = {
+        {
+            desc = 'increase window sticking to left top',
+            before = { x = 0, y = 0, w = 600, h = 400 },
+            expect = { x = 0, y = 0, w = 700, h = 466 },
+            increase = true,
+        },
+        {
+            desc = 'decrease window sticking to left top',
+            before = { x = 0, y = 0, w = 600, h = 400 },
+            expect = { x = 0, y = 0, w = 500, h = 334 },
+            increase = false,
+        },
+        {
+            desc = 'increase window sticking to right top',
+            before = { x = 600, y = 0, w = 600, h = 400 },
+            expect = { x = 500, y = 0, w = 700, h = 466 },
+            increase = true,
+        },
+        {
+            desc = 'decrease window sticking to right top',
+            before = { x = 600, y = 0, w = 600, h = 400 },
+            expect = { x = 700, y = 0, w = 500, h = 334 },
+            increase = false,
+        },
+        {
+            desc = 'increase window sticking to left bottom',
+            before = { x = 0, y = 400, w = 600, h = 400 },
+            expect = { x = 0, y = 334, w = 700, h = 466 },
+            increase = true,
+        },
+        {
+            desc = 'decrease window sticking to left bottom',
+            before = { x = 0, y = 400, w = 600, h = 400 },
+            expect = { x = 0, y = 466, w = 500, h = 334 },
+            increase = false,
+        },
+        {
+            desc = 'increase window sticking to right bottom',
+            before = { x = 0, y = 400, w = 600, h = 400 },
+            expect = { x = 0, y = 334, w = 700, h = 466 },
+            increase = true,
+        },
+        {
+            desc = 'decrease window sticking to right bottom',
+            before = { x = 600, y = 400, w = 600, h = 400 },
+            expect = { x = 700, y = 466, w = 500, h = 334 },
+            increase = false,
+        },
+    }
+    for _, test in pairs(tests) do
+        hsmocks.window.rect = test.before
+        shiftit:resizeWindowInSteps(test.increase)
+        lu.assertEquals(hsmocks.window.rect, test.expect, test.desc)
     end
 end
 
